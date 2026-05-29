b35d03804ee5aacb7a041d2ce34a90b061afebd4
陳奕宇2025\aaron <aaron.chen@fstshafts.com.tw>
2026-05-29 15:44:02 +0800

    refactor(storage): 統一 LocalStorage 鍵值命名為 anispace 前綴

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const NSFW_GENRES = ['Hentai', 'Ecchi', 'Erotica', 'Boys Love', 'Girls Love', '耽美', '百合', '福利'];
 export const ITEMS_PER_PAGE = 25;
-export const LOCAL_STORAGE_KEY = 'anime_manager_watched_list';
-export const PLAN_TO_WATCH_KEY = 'anime_manager_plan_to_watch';
-export const CACHED_DATA_KEY = 'anime_manager_cached_data';
-export const TITLE_CORRECTIONS_KEY = 'anime_manager_title_corrections';
+export const LOCAL_STORAGE_KEY = 'anispace_watched';
+export const PLAN_TO_WATCH_KEY = 'anispace_plan_to_watch';
+export const CACHED_DATA_KEY = 'anispace_cached_data';
+export const TITLE_CORRECTIONS_KEY = 'anispace_title_corrections';
+export const CUSTOM_ANIME_KEY = 'anispace_custom_anime';
